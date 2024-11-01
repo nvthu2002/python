46ac07cd3e04897234fc75f6883c787197ca9138
nvthu2002 <nvthu2002@gmail.com>
2024-11-01 17:46:37 +0700

    Delete gomtukhoa directory

--- gomtukhoa/keyword_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KeywordAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'keyword_app'
--- gomtukhoa/keyword_app/urls.py
+++
@@ -1,8 +0,0 @@
-# keyword_app/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_file, name='upload_file'),
-    path('result/', views.result_view, name='result_view'),
-]
